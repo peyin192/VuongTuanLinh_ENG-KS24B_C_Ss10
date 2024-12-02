ed2f4070650f599fd8436a3516dd18f1b71a64de
peyin192 <vuonglinh190206.qv@gmail.com>
2024-12-03 00:05:24 +0700

    upload ss10-8

---
+++ ex08.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	
+	printf("Nhap so dong: ");
+	scanf("%d",&a);
+	printf("Nhap so cot: ");
+	scanf("%d",&b);
+	
+	int arr[a][b];
+	
+	printf("Nhap cac phan tu trong mang: ");
+	for (int i=0;i<a;i++){
+		for(int j=0;j<b;j++){
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	int column[a],i,j;
+	for (int i = 0; i < a; i++) {
+            arr[i][j] = column[i];
+        }
+        
+	for (int i=0;i<a;i++) {
+        for (int j=0;j<b;j++) {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+	
+	
+	
+}
